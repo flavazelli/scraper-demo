6dca86c44b6898368386352116bf00a4d3f5586b
Francis Lavazelli <francis.lavazelli@gmail.com>
2025-03-26 13:56:16 -0400

    added aps task scheduler

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from typing import Union, Any
 from fastapi import FastAPI, status
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+import time
+import atexit
 from pydantic import BaseModel
 from langchain_chain import query_bot
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,23 +18,26 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+scheduler = BackgroundScheduler()
+
+#TODO: refactor scraper so they can launch once a day
+
+def print_time():
+    print(f"The current time is {time.ctime()}")
+
+scheduler.add_job(print_time, CronTrigger(minute="*/1")) 
+scheduler.start()
+
+@atexit.register
+def shutdown():
+    scheduler.shutdown()
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
-    return {"Hello": "World"}
-
-
+    return {"status" : "ok"}
 
 @app.post("/question")
 def ask_question(question: Question, status_code=status.HTTP_200_OK):
     response = query_bot(question.question)
     return  {"response": response}
-    
-
-
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
